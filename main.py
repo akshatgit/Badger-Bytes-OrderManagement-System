6dab62fb9b36460dcdab25ce32f75ac3e6bc1b2c
Ping Qu <pqu6@wisc.edu>
2021-02-24 08:40:45 -0600

    add admin log in

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
@@ -9,6 +9,10 @@ def index():
 @app.route('/signup')
 def signup():
     return render_template("signup.html")
+
+@app.route('/adminLogin')
+def adminLogin():
+    return render_template("adminLogin.html")
 
 if __name__ == '__main__':
     app.run()
